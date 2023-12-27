aa29d8229ab48342e2ddf37145d8b929a44ce7f0
Fatmatrn <ftmtrn3488@gmail.com>
2023-12-27 15:28:36 +0300

    opengraph

--- app/components/header1.tsx
+++ app/components/header1.tsx
@@ -1,16 +1,35 @@
 import Link from 'next/link'
 import React from 'react'
+import { Github,Linkedin } from 'lucide-react';
+
+import SocialLink from './SocialLink'
+
+const socials =[
+  {label:'Github',
+  icon:Github,
+  href:'https://github.com/fatmatrn'},
+  {label:'LinkedIn',
+  icon:Linkedin,
+  href:'https://www.linkedin.com/in/fatmatrn'},
+]
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
-    <header className='flex items-center justify-end w-full max-w-3xl px-4 py-8 mx-auto'>
+    <header className='flex items-center justify-end space-x-5 w-full max-w-3xl px-4 py-8 mx-auto'>
 
     <nav className='flex  items-center space-x-4'>
       <Link href='/'  className='font-medium'>About</Link>
       <Link href='/'  className='font-medium'>Blog</Link>
     </nav>
+    <div className="flex items-center space-x-4  ">
+            {socials.map((social)=>(
+                <SocialLink key={social.label}
+                icon={social.icon}
+                href={social.href}/>
+            ))}
+        </div>
  
   </header>
   )
